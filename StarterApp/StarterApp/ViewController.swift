2778eff86fea2b7b4feee3982464a6680e782e46
ccanadas <carles.canadas.torrents@everis.com>
2020-02-01 17:36:28 +0100

    [Initial Commit]

---
+++ StarterApp/StarterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarterApp
+//
+//  Created by Carles Cañadas Torrents on 01/02/2020.
+//  Copyright © 2020 karlesct. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UIElements/UIElements/UIElements.h
@@ -0,0 +1,19 @@
+//
+//  UIElements.h
+//  UIElements
+//
+//  Created by Carles Cañadas Torrents on 01/02/2020.
+//  Copyright © 2020 karlesct. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIElements.
+FOUNDATION_EXPORT double UIElementsVersionNumber;
+
+//! Project version string for UIElements.
+FOUNDATION_EXPORT const unsigned char UIElementsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIElements/PublicHeader.h>
+
+
