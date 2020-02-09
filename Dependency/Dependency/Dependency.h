85f3f12703d55eee433d7f3d4c38cc50464f2f9e
ccanadas <carles.canadas.torrents@everis.com>
2020-02-09 18:25:40 +0100

    [INSERT] - Dependency framework

---
+++ Dependency/Dependency/Dependency.h
@@ -0,0 +1,19 @@
+//
+//  Dependency.h
+//  Dependency
+//
+//  Created by Carles Cañadas Torrents on 09/02/2020.
+//  Copyright © 2020 karlesct. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Dependency.
+FOUNDATION_EXPORT double DependencyVersionNumber;
+
+//! Project version string for Dependency.
+FOUNDATION_EXPORT const unsigned char DependencyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dependency/PublicHeader.h>
+
+
