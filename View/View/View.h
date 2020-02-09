6252997c0f6a5982ce8be7ac9f1fa86accc3ef3e
ccanadas <carles.canadas.torrents@everis.com>
2020-02-09 17:23:32 +0100

    [INSERT] - Add View project

---
+++ View/View/View.h
@@ -0,0 +1,19 @@
+//
+//  View.h
+//  View
+//
+//  Created by Carles Cañadas Torrents on 09/02/2020.
+//  Copyright © 2020 karlesct. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for View.
+FOUNDATION_EXPORT double ViewVersionNumber;
+
+//! Project version string for View.
+FOUNDATION_EXPORT const unsigned char ViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <View/PublicHeader.h>
+
+
